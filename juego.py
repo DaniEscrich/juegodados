94c188954fcfd5dda5091db82d6234559d80f72a
Daniel <daniescrich24@gmail.com>
2024-05-29 00:33:49 +0200

    V7

--- juego.py
+++ juego.py
@@ -22,29 +22,29 @@ class Juego:
         self.resultado3 = 0
 
         if (caras1 == caras2 or caras1 == caras3 or caras1 == caras4 or caras2 == caras3 or caras2 == caras4 or caras3 == caras4):
-                raise Exception ("Error caras iguales")
+                raise Exception ("Errors cares iguals")
 
     def set_jugador1(self, fjugador1):
         if len(fjugador1) > 20:
-            raise Exception("La longitud del nombre del jugador 1 no puede ser mayor de 20")
+            raise Exception("La longitud del nom del jugador 1 no pot ser major de 20")
         else:
             self.__jugador1 = fjugador1
 
     def set_jugador2(self, fjugador2):
         if len(fjugador2) > 20:
-            raise Exception("La longitud del nombre del jugador 2 no puede ser mayor de 20")
+            raise Exception("La longitud del nom del jugador 2 no pot ser major de 20")
         else:
             self.__jugador2 = fjugador2
 
     def set_jugador3(self, fjugador3):
         if len(fjugador3) > 20:
-            raise Exception("La longitud del nombre del jugador 3 no puede ser mayor de 20")
+            raise Exception("La longitud del nom del jugador 3 no pot ser major de 20")
         else:
             self.__jugador3 = fjugador3
 
     def set_lanzamientos(self, lanzamientos):
         if not 2 < lanzamientos <= 1000:
-            raise Exception("El número de lanzamientos debe de estar entre 2 y 1000")
+            raise Exception("El nombre de llançaments han d'estar entre 2 i 1000")
         else:
             self.__lanzamientos = lanzamientos
 
@@ -59,7 +59,7 @@ class Juego:
             self.resultado1 += (ptsTirada1 + ptsTirada2 + ptsTirada3 + ptsTirada4)
             if self.__intermedios:
                 print(
-                    f"Lanzamiento {x + 1}: \n {self.__jugador1}: {ptsTirada1} {ptsTirada2} {ptsTirada3} {ptsTirada4} ({(ptsTirada1 + ptsTirada2 + ptsTirada3 + ptsTirada4)})")
+                    f"Llançament {x + 1}: \n {self.__jugador1}: {ptsTirada1} {ptsTirada2} {ptsTirada3} {ptsTirada4} ({(ptsTirada1 + ptsTirada2 + ptsTirada3 + ptsTirada4)})")
 
             ptsTirada1 = self.dado1.lanzar()
             ptsTirada2 = self.dado2.lanzar()
@@ -78,13 +78,13 @@ class Juego:
                 print(f"{self.__jugador3}: {ptsTirada1} {ptsTirada2} {ptsTirada3} ({(ptsTirada1 + ptsTirada2 + ptsTirada3 + ptsTirada4)}) \n")
 
     def mostrar(self):
-        print(f"Resultados: \n Jugador 1: {self.__jugador1} \n Jugador 2: {self.__jugador2} \n Jugador 3: {self.__jugador3} \n Numero de lanzamientos: {self.__lanzamientos} ")
-        print(f"Dados: {self.dado1.getCaras()},{self.dado2.getCaras()} y {self.dado3.getCaras()} \n Puntos jugador 1: {self.resultado1} \n Puntos jugador 2: {self.resultado2} \n Puntos jugador 3: {self.resultado3}")
+        print(f"Resultats: \n Jugador 1: {self.__jugador1} \n Jugador 2: {self.__jugador2} \n Jugador 3: {self.__jugador3} \n Nombre de Llançaments: {self.__lanzamientos} ")
+        print(f"Daus: {self.dado1.getCaras()},{self.dado2.getCaras()} i {self.dado3.getCaras()} \n Punts jugador 1: {self.resultado1} \n Punts jugador 2: {self.resultado2} \n Punts jugador 3: {self.resultado3}")
         if self.resultado1 > self.resultado2 and self.resultado1 > self.resultado3:
-            print(f"El GANADOR es {self.__jugador1} con {self.resultado1} puntos")
+            print(f"El GUANYADOR es {self.__jugador1} amb {self.resultado1} punts")
         elif self.resultado2 > self.resultado1 and self.resultado2 > self.resultado3:
-            print(f"El GANADOR es {self.__jugador2} con {self.resultado2} puntos")
+            print(f"El GUANYADOR es {self.__jugador2} amb {self.resultado2} punts")
         elif self.resultado3 > self.resultado1 and self.resultado3 > self.resultado2:
-            print(f"El GANADOR es {self.__jugador3} con {self.resultado3} puntos")
+            print(f"El GUANYADOR es {self.__jugador3} amb {self.resultado3} punts")
         else:
-            print(f"EMPATEE!!!")
+            print(f"EMPAT!!!")
