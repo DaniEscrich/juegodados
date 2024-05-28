38ece2a150af94e150771c525da45f8a0850d16e
Daniel <daniescrich24@gmail.com>
2024-05-28 23:49:36 +0200

    T1

--- juego.py
+++ juego.py
@@ -4,11 +4,13 @@ import dado
 class Juego:
     __jugador1 = ""
     __jugador2 = ""
+    __jugador3 = ""
     __lanzamientos = 0
 
-    def __init__(self, jugador1, jugador2, caras1, caras2, caras3, lanzamientos, intermedios):
+    def __init__(self, jugador1, jugador2, jugador3, caras1, caras2, caras3, lanzamientos, intermedios):
         self.set_jugador1(jugador1)
         self.set_jugador2(jugador2)
+        self.set_jugador3(jugador3)
         self.set_lanzamientos(lanzamientos)
         self.dado1 = dado.Dado(caras1)
         self.dado2 = dado.Dado(caras2)
@@ -16,6 +18,7 @@ class Juego:
         self.__intermedios = (intermedios in ("S", "s"))
         self.resultado1 = 0
         self.resultado2 = 0
+        self.resultado3 = 0
 
     def set_jugador1(self, fjugador1):
         if len(fjugador1) > 20:
@@ -24,10 +27,16 @@ class Juego:
             self.__jugador1 = fjugador1
 
     def set_jugador2(self, fjugador2):
-        if len(fjugador2) > 20 and len(fjugador2) > 20 and len(fjugador2) > 20:
+        if len(fjugador2) > 20:
             raise Exception("La longitud del nombre del jugador 2 no puede ser mayor de 20")
         else:
             self.__jugador2 = fjugador2
+
+    def set_jugador3(self, fjugador3):
+        if len(fjugador3) > 20:
+            raise Exception("La longitud del nombre del jugador 3 no puede ser mayor de 20")
+        else:
+            self.__jugador3 = fjugador3
 
     def set_lanzamientos(self, lanzamientos):
         if not 2 < lanzamientos <= 1000:
@@ -36,16 +45,13 @@ class Juego:
             self.__lanzamientos = lanzamientos
 
     def jugar(self):
-        self.resultado1 = 0
-        self.resultado2 = 0
         for x in range(self.__lanzamientos):
             ptsTirada1 = self.dado1.lanzar()
             ptsTirada2 = self.dado2.lanzar()
             ptsTirada3 = self.dado3.lanzar()
             self.resultado1 += (ptsTirada1 + ptsTirada2 + ptsTirada3)
             if self.__intermedios:
-                print(
-                    f"Lanzamiento {x + 1}: \n {self.__jugador1}: {ptsTirada1} {ptsTirada2} {ptsTirada3} ({(ptsTirada1 + ptsTirada2 + ptsTirada3)})")
+                print(f"Lanzamiento {x + 1}: \n {self.__jugador1}: {ptsTirada1} {ptsTirada2} {ptsTirada3} ({(ptsTirada1 + ptsTirada2 + ptsTirada3)})")
 
             ptsTirada1 = self.dado1.lanzar()
             ptsTirada2 = self.dado2.lanzar()
@@ -53,14 +59,18 @@ class Juego:
             self.resultado2 += (ptsTirada1 + ptsTirada2 + ptsTirada3)
 
             if self.__intermedios:
-                print(
-                    f"{self.__jugador2}: {ptsTirada1} {ptsTirada2} {ptsTirada3} ({(ptsTirada1 + ptsTirada2 + ptsTirada3)}) \n")
+                print(f"{self.__jugador2}: {ptsTirada1} {ptsTirada2} {ptsTirada3} ({(ptsTirada1 + ptsTirada2 + ptsTirada3)})")
+
+            ptsTirada1 = self.dado1.lanzar()
+            ptsTirada2 = self.dado2.lanzar()
+            ptsTirada3 = self.dado3.lanzar()
+            self.resultado3 += (ptsTirada1 + ptsTirada2 + ptsTirada3)
+            if self.__intermedios:
+                print(f"{self.__jugador3}: {ptsTirada1} {ptsTirada2} {ptsTirada3} ({(ptsTirada1 + ptsTirada2 + ptsTirada3)}) \n")
 
     def mostrar(self):
-        print(
-            f"Resultados: \n Jugador 1: {self.__jugador1} \n Jugador 2: {self.__jugador2}\n Numero de lanzamientos: {self.__lanzamientos} ")
-        print(
-            f"Dados: {self.dado1.getCaras()},{self.dado2.getCaras()} y {self.dado3.getCaras()} \n Puntos jugador 1: {self.resultado1} \n Puntos jugador 2: {self.resultado2}")
+        print(f"Resultados: \n Jugador 1: {self.__jugador1} \n Jugador 2: {self.__jugador2}\n Numero de lanzamientos: {self.__lanzamientos} ")
+        print(f"Dados: {self.dado1.getCaras()},{self.dado2.getCaras()} y {self.dado3.getCaras()} \n Puntos jugador 1: {self.resultado1} \n Puntos jugador 2: {self.resultado2}")
         if self.resultado1 > self.resultado2:
             print(f"El GANADOR es {self.__jugador1} con {self.resultado1} puntos")
         elif self.resultado1 == self.resultado2:
